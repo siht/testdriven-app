c66b631d85891c54748730a44f35138b5e3d9f1e
carlos <radix_10339@hotmail.com>
2019-10-06 21:17:48 -0500

    add user model

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -8,8 +9,25 @@ app = Flask(__name__)
 
 api = Api(app)
 
+# set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 class UsersPing(Resource):
     def get(self):
